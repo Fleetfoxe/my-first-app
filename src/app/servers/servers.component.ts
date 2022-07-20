f59d6b27e3553a1a4790ded6ceabcf4df5cc38c3
Peter <p.akesson@gmail.com>
2022-07-20 21:15:25 +0200

    removing old code

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -6,15 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
-  allowNewServer = false;
-  serverCreationStatus = 'No server was created';
   userName = '';
 
-  constructor() { 
-    setTimeout(() => {
-      this.allowNewServer = true;
-    }, 2000);
-  }
+  constructor() {}
 
   ngOnInit(): void {
   }
