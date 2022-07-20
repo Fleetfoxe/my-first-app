5f71ede511b92455e793e0cd02b51b9d1c3f17ac
Peter <p.akesson@gmail.com>
2022-07-20 21:07:36 +0200

    button disabled when username is empty string

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
-  userName = 'User name';
+  userName = '';
 
   constructor() { 
     setTimeout(() => {
@@ -18,8 +18,8 @@ export class ServersComponent implements OnInit {
 
   ngOnInit(): void {
   }
-  onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+  onResertUserName() {
+    this.userName = '';
   }
   
 }
