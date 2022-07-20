92eea26a09d3bf4ce0d42d26fbda1dd8696033da
Peter <p.akesson@gmail.com>
2022-07-20 21:28:57 +0200

    Removing old code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,18 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
-import { SuccessalertComponent } from './successalert/successalert.component';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ServerComponent,
-    ServersComponent,
-    SuccessalertComponent
   ],
   imports: [
     BrowserModule,
